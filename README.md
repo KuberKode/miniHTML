b2a3f38874712fd5697715345997ed3b285fd639
Johan Strydom <fyrewolfe@gmail.com>
2022-09-12 08:26:59 +0200

    Some refactroring
    - Refactoring on server.js
    - Tested with nodejs v16.17.0 LTS
    - Add some more text
    - Add link back to github

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A mini HTML node and document class for nodejs with basic, qualified, CSS rules
 
 This software is released into the public domain
 
-`Tested with nodejs v12.18.3 LTS`
+`Tested with nodejs v16.17.0 LTS`
 
 To start, run the following command:
 
--- server.js
+++ server.js
@@ -7,28 +7,29 @@
 */
 const http = require('http');
 
-var HTMLDocument = require('./htmldocument');
-var HTMLStyle = require('./htmlstyle');
+const HTMLDocument = require('./htmldocument');
+const HTMLStyle = require('./htmlstyle');
 
-var listenPort = 8080;
+let listenPort = 8080;
+const nodeVersionTested = "v16.17.0 LTS";
 
 const server = http.createServer((request, response) => {
 	
-	var doc = new HTMLDocument({'debug':false});
+	const doc = new HTMLDocument({'debug':false});
 	
-	var title = doc.head.addChild("title");
-	title.setText("Server");
+	const title = doc.head.addChild("title");
+	title.setText("miniHTML Server");
 	
-	var style = doc.head.addChild("style");
+	const style = doc.head.addChild("style");
 	style.addAttribute("type","text/css");
 	
-	var bodyStyle = new HTMLStyle();
-	var bodyRules = bodyStyle.addRule("body");
+	const bodyStyle = new HTMLStyle();
+	const bodyRules = bodyStyle.addRule("body");
 
 	bodyRules.addDeclaration("font-family","Arial");
 	bodyRules.addDeclaration("padding","0");
 	
-	var aStyle = new HTMLStyle({"a":{
+	const aStyle = new HTMLStyle({"a":{
 		"display":"block",
 		"border-radius":"6px",
 		"color":"#FFFFFF",
@@ -43,18 +44,32 @@ const server = http.createServer((request, response) => {
 	
 	style.setText(bodyStyle.toString()+aStyle.toString());
 	
-	var h1 = doc.body.addChild("h1");
+	const h1 = doc.body.addChild("h1");
 	h1.setText("Hello World");
-	var h1Rule = h1.nodeStyle.addRule("h1");
+	const h1Rule = h1.nodeStyle.addRule("h1");
 	h1Rule.addDeclaration("color","#FF0000");
 	h1Rule.addDeclaration("size","200%");
 	
-	var p = doc.body.addChild("p");
+	const h3 = doc.body.addChild("h3");		
+	h3.setText("Server is listening at http://localhost:"+listenPort);
 	
-	var a = p.addChild("a");
+	const p1 = doc.body.addChild("p");	
+	p1.setText("A mini HTML node and document class for nodejs with basic, qualified, CSS rules");
+
+	const p2 = doc.body.addChild("p");
+	p2.setText("This software is released into the public domain");
+
+	const p3 = doc.body.addChild("p");
+	p3.setText("Tested with nodejs "+nodeVersionTested);
+
+	const p4 = doc.body.addChild("p");
+	p4.setText("Disclaimer: Not to be associated with SublimeText minihtml");
 	
-	a.addAttribute("href","http://localhost:" + listenPort);
-	a.setText("Click me!");
+	const p5 = doc.body.addChild("p");
+	var a = p5.addChild("a");	
+	a.addAttribute("href","https://github.com/KuberKode/miniHTML");
+	a.addAttribute("target","_github");	
+	a.setText("View on GitHub");
 	
 	response.writeHead(200, {"Content-Type": "text/html"});	
 	response.write(doc.toString());
